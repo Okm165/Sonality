9046338e56ee5bc1e3477d2799554c31511041e8
Bartosz Nowak <bartekn337@gmail.com>
2026-04-26 21:11:09 +0200

    Simplify memory module public API
    Update __all__ exports:
    - Add: SEED_SNAPSHOT, BeliefNode, PersonalitySnapshot, chain_retrieve,
      route_query, split_retrieve
    - Remove: deleted module exports (SpongeState, ShortTermMemory,
      BackgroundSummarizer, HealthReport, etc.)
    - Remove: internal implementation details (ContractionAction,
      UpdateMagnitude, AggregationStrategy, etc.)

--- sonality/memory/__init__.py
+++ sonality/memory/__init__.py
@@ -1,109 +1,52 @@
 from __future__ import annotations
 
-from .belief_provenance import (
-    ContractionAction,
-    ProvenanceUpdate,
-    UpdateMagnitude,
-    assess_belief_evidence,
-    assess_belief_evidence_batch,
-)
-from .consolidation import ConsolidationEngine, ConsolidationReadinessDecision
+from .belief_provenance import assess_belief_evidence_batch
 from .db import DatabaseConnections
-from .derivatives import ChunkImportance, DerivativeChunker
-from .dual_store import DualEpisodeStore, EpisodeStorageError, StoredEpisode
-from .embedder import Embedder, EmbeddingUnavailableError, cosine_similarity
-from .forgetting import ForgettingAction, ForgettingEngine
-from .graph import BeliefCorrelation, EdgeType, EpisodeNode, MemoryGraph
-from .health import HealthReport, OverallHealth, assess_health
-from .health_trace import dump_memory_snapshot, trace_belief_provenance
-from .knowledge_extract import (
-    DetectedCorrelation,
-    ExtractedProposition,
-    KnowledgeConsolidation,
-    PropositionType,
-    consolidate_knowledge,
-    detect_correlations,
-    extract_and_store_knowledge,
-    prune_stale_knowledge,
-    retrieve_relevant_knowledge,
+from .dual_store import DualEpisodeStore, StoredEpisode
+from .embedder import Embedder
+from .graph import (
+    SEED_SNAPSHOT,
+    BeliefNode,
+    EdgeType,
+    EpisodeNode,
+    MemoryGraph,
+    PersonalitySnapshot,
 )
+from .knowledge_extract import extract_and_store_knowledge, retrieve_relevant_knowledge
 from .retrieval import (
-    AggregationStrategy,
-    ChainOfQueryAgent,
     QueryCategory,
-    QueryRouter,
-    RoutingDecision,
     SemanticMemoryDecision,
-    SplitQueryAgent,
-    SufficiencyDecision,
     TemporalExpansionDecision,
+    chain_retrieve,
     rerank_episodes,
+    route_query,
+    split_retrieve,
 )
-from .segmentation import BoundaryDecision, BoundaryResult, BoundaryType, EventBoundaryDetector
+from .segmentation import BoundaryDecision, EventBoundaryDetector
 from .semantic_features import SemanticIngestionWorker
-from .sponge import BeliefMeta, BeliefState, ProbabilityEstimate, SpongeState, StagedOpinionUpdate
-from .stm import ShortTermMemory
-from .stm_consolidator import BackgroundSummarizer
-from .updater import extract_insight
 
 __all__ = [
-    "AggregationStrategy",
-    "BackgroundSummarizer",
-    "BeliefCorrelation",
-    "BeliefMeta",
-    "BeliefState",
+    "SEED_SNAPSHOT",
+    "BeliefNode",
     "BoundaryDecision",
-    "BoundaryResult",
-    "BoundaryType",
-    "ChainOfQueryAgent",
-    "ChunkImportance",
-    "ConsolidationEngine",
-    "ConsolidationReadinessDecision",
-    "ContractionAction",
     "DatabaseConnections",
-    "DerivativeChunker",
-    "DetectedCorrelation",
     "DualEpisodeStore",
     "EdgeType",
     "Embedder",
-    "EmbeddingUnavailableError",
     "EpisodeNode",
-    "EpisodeStorageError",
     "EventBoundaryDetector",
-    "ExtractedProposition",
-    "ForgettingAction",
-    "ForgettingEngine",
-    "HealthReport",
-    "KnowledgeConsolidation",
     "MemoryGraph",
-    "OverallHealth",
-    "ProbabilityEstimate",
-    "PropositionType",
-    "ProvenanceUpdate",
+    "PersonalitySnapshot",
     "QueryCategory",
-    "QueryRouter",
-    "RoutingDecision",
     "SemanticIngestionWorker",
     "SemanticMemoryDecision",
-    "ShortTermMemory",
-    "SplitQueryAgent",
-    "SpongeState",
-    "StagedOpinionUpdate",
     "StoredEpisode",
-    "SufficiencyDecision",
     "TemporalExpansionDecision",
-    "UpdateMagnitude",
-    "assess_belief_evidence",
     "assess_belief_evidence_batch",
-    "assess_health",
-    "consolidate_knowledge",
-    "cosine_similarity",
-    "detect_correlations",
-    "dump_memory_snapshot",
+    "chain_retrieve",
     "extract_and_store_knowledge",
-    "extract_insight",
-    "prune_stale_knowledge",
     "rerank_episodes",
     "retrieve_relevant_knowledge",
-    "trace_belief_provenance",
+    "route_query",
+    "split_retrieve",
 ]
